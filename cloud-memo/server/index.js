a39bbe4ed7d4370f3f8183d1d0f31f4fbf7dd62e
se030 <kimsy30@g.skku.edu>
2022-07-12 10:36:46 +0900

    response formatted

--- cloud-memo/server/index.js
+++ cloud-memo/server/index.js
@@ -11,6 +11,7 @@ const save = () => fs.writeFileSync("data.json", JSON.stringify(data), "utf-8");
 const isValidId = (id, res) => {
     if (isNaN(id) || id < 0 || data.length <= id) {
         res.status(400).json({
+            "rs": false,
             "msg": "잘못된 id 입니다."
         })
         return false;
@@ -18,8 +19,9 @@ const isValidId = (id, res) => {
     return true
 }
 const isValidContent = (content, res, tempContent=false) => {
-    if (!content || (!tempContent && content?.length === 0)) {
+    if (!tempContent && (!content || content?.length === 0)) {
         res.status(400).json({
+            "rs": false,
             "msg": "메모 내용이 올바르지 않습니다."
         })
         return false;
@@ -31,12 +33,16 @@ let temp = '';
 
 app.get('/', (req, res) => res.json(data.filter(memo => memo.deleted_at === null)))
 app.get('/trash', (req, res) => res.json(data.filter(memo => memo.deleted_at)))
-app.get('/temp', (req, res) => res.json(temp))
+app.get('/temp', (req, res) => res.json({
+    "rs": true,
+    "content": temp
+}))
 app.get('/:id', (req, res) => {
     const id = parseInt(req.params.id, res);
     if (isValidId(id, res)) {
         if (data[id].deleted_at) {
             res.status(400).json({
+                "rs": false,
                 "msg": "잘못된 id 입니다."
             })
         }
@@ -48,14 +54,20 @@ app.delete('/', (req, res) => {
     for (let memo of data)
         if (!memo.deleted_at) memo.deleted_at = Date.now()
     save();
-    res.json({msg: "삭제되었습니다."})
+    res.json({
+        rs: "true", 
+        msg: "삭제되었습니다."
+    })
 })
 app.delete('/:id', (req, res) => {
     const id = parseInt(req.params.id, res);
     if (isValidId(id, res)) {
         data[id].deleted_at = Date.now();
         save();
-        res.json({msg: "삭제되었습니다."})
+        res.json({
+            rs: "true", 
+            msg: "삭제되었습니다."
+        })
     }
 })
 
